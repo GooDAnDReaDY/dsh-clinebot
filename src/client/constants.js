1ac26b2b8a317b8706e7c7fe8a172634191c6b7a
GooDAnDReaDY <vadim@goodandready.app>
2026-09-23 13:48:51 +0300

    chore(publish): sanitized tree from b2e61d2e

--- src/client/constants.js
+++
@@ -1,6 +0,0 @@
-const NS = 'dsh-clinebot'
-// Plugins page row seat (DSH 0.1.6-alpha.2): key = '<package name>#<row id>'.
-const ROW_ID = 'dsh-clinebot'
-const ROW_CONFIG_KEY = '@goodandready/dsh-clinebot#' + ROW_ID
-const ROUTE_PREFIX = '/dsh-clinebot'
-const SNAPSHOT_LOADING = { status: 'loading', view: null }
